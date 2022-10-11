ba3ee11149f254ea3f8ce909f82bacc6b23876fd
Fellipe Brandao <fellipesilverio31@hotmail.com>
2022-10-10 22:41:49 -0300

    feat: ajustando cron

--- src/job/CronJob.ts
+++ src/job/CronJob.ts
@@ -2,34 +2,42 @@ import { CronJob } from 'cron';
 import { GetFiis } from '../modules/fundos_imobiliarios/useCases/Fiis/GetFiis';
 import { container } from "tsyringe";
 import { FiisUseCase } from '../modules/fundos_imobiliarios/useCases/Fiis/FiisUseCase';
-import { ICreateFiisDTOS } from '../modules/fundos_imobiliarios/dtos/ICreateFiisDTOS';
 
 class Jobs {
+    cronJob: CronJob;
 
-  cronJob: CronJob;
+    constructor() {
+        console.log("Jobs is running");
 
-  constructor() {
-    console.log("Jobs is running");
+        this.cronJob = new CronJob('30 * * * * *', async () => {
+            try {
+                await this.getInfoFundos();
+                await this.processaDados();
+            } catch (e) {
+                console.error(e);
+            }
+        });
 
-    this.cronJob = new CronJob('1 * * * * *', async () => {
-      try {
-        await this.getInfoFundos();
-      } catch (e) {
-        console.error(e);
-      }
-    });
-    
-    // Start job
-    if (!this.cronJob.running) {
-      this.cronJob.start();
+        // Start job
+        if (!this.cronJob.running) {
+            this.cronJob.start();
+        }
     }
-  }
-   async getInfoFundos() {
-      let getFiis = new GetFiis;
-      let fundos = await getFiis.execute();
 
-      const fiisUseCase = container.resolve(FiisUseCase);
-      await fiisUseCase.update_fiis(fundos);
+    async getInfoFundos() {
+        let getFiis = new GetFiis;
+        let fundos = await getFiis.execute();
+
+        if (fundos) {
+            const fiisUseCase = container.resolve(FiisUseCase);
+            await fiisUseCase.update_fiis(fundos);
+        } else {
+            console.log('Dados nao atualizados!');
+        }
+    }
+
+    async processaDados() {
+
     }
 
 }
