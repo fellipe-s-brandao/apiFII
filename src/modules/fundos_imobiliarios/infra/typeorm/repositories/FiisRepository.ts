22f3a477a4352344889a3af907355975c62c7770
Fellipe Brandao <fellipesilverio31@hotmail.com>
2022-11-04 23:44:43 -0300

    feat: melhorando query do relatorio

--- src/modules/fundos_imobiliarios/infra/typeorm/repositories/FiisRepository.ts
+++ src/modules/fundos_imobiliarios/infra/typeorm/repositories/FiisRepository.ts
@@ -14,17 +14,20 @@ class FiisRepository implements IFiisRepository {
     async getBestFiisQuery1(): Promise<Fiis[]> {
         const data = await
             this.repository.createQueryBuilder('fiis')
-                .where('fiis.dy > :dy', { dy: process.env.BEST1_dy })
-                .andWhere('fiis.rentabilidade_periodo > :rentabilidade_periodo', { rentabilidade_periodo: process.env.BEST1_rentabilidade_periodo })
-                .andWhere('fiis.rentabilidade_acumulada > :rentabilidade_acumulada', { rentabilidade_acumulada: process.env.BEST1_rentabilidade_acumulada })
-                .andWhere('fiis.p_vpa < :p_vpa', { p_vpa: process.env.BEST1_p_vpa })
-                .andWhere('fiis.variacao_patrimonial > :variacao_patrimonial', { variacao_patrimonial: process.env.BEST1_variacao_patrimonial })
-                .andWhere('fiis.rentabilidade_patrimonial_periodo > :rentabilidade_patrimonial_periodo', { rentabilidade_patrimonial_periodo: process.env.BEST1_rentabilidade_patrimonial_periodo })
-                .andWhere('fiis.rentabilidade_patrimonial_acumulada > :rentabilidade_patrimonial_acumulada', { rentabilidade_patrimonial_acumulada: process.env.BEST1_rentabilidade_patrimonial_acumulada })
+                .where('fiis.dy >= :dy', { dy: process.env.BEST1_dy })
+                .andWhere('fiis.rentabilidade_periodo >= :rentabilidade_periodo', { rentabilidade_periodo: process.env.BEST1_rentabilidade_periodo })
+                .andWhere('fiis.rentabilidade_acumulada >= :rentabilidade_acumulada', { rentabilidade_acumulada: process.env.BEST1_rentabilidade_acumulada })
+                .andWhere('fiis.p_vpa <= :p_vpa', { p_vpa: process.env.BEST1_p_vpa })
+                .andWhere('fiis.variacao_patrimonial >= :variacao_patrimonial', { variacao_patrimonial: process.env.BEST1_variacao_patrimonial })
+                .andWhere('fiis.rentabilidade_patrimonial_periodo >= :rentabilidade_patrimonial_periodo', { rentabilidade_patrimonial_periodo: process.env.BEST1_rentabilidade_patrimonial_periodo })
+                .andWhere('fiis.rentabilidade_patrimonial_acumulada >= :rentabilidade_patrimonial_acumulada', { rentabilidade_patrimonial_acumulada: process.env.BEST1_rentabilidade_patrimonial_acumulada })
                 .andWhere('fiis.vacancia_financeira = :vacancia_financeira', { vacancia_financeira: process.env.BEST1_vacancia_financeira })
-                .andWhere('fiis.vacancia_fisica = :vacancia_fisica', { vacancia_fisica: process.env.BEST1_vacancia_fisica })
-                .andWhere('fiis.liquidez_diaria > :liquidez_diaria', { liquidez_diaria: process.env.BEST1_liquidez_diaria })
+                .andWhere('fiis.vacancia_fisica <= :vacancia_fisica', { vacancia_fisica: process.env.BEST1_vacancia_fisica })
+                .andWhere('fiis.liquidez_diaria >= :liquidez_diaria', { liquidez_diaria: process.env.BEST1_liquidez_diaria })
+                .andWhere('abs(fiis.dy - fiis.dy_12m_media) <= :diferenca_entre_medias ', { diferenca_entre_medias : process.env.BEST1_diferenca_entre_medias })
+                .andWhere('fiis.dy_12m_media >= :dy_12m_media ', { dy_12m_media : process.env.BEST1_dy_12m_media })
                 .orderBy('fiis.dy', "DESC")
+                .limit(null)
                 .getMany();
 
         return data;
